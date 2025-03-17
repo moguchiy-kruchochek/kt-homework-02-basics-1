b6ba81778647ea6313faea459e9ca22f568674cb
Jaroslav Filatov <starayaglina@gmail.com>
2025-03-17 21:46:15 +0700

    comission.toInt() without round

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,13 @@
 package ru.netology
 
-import kotlin.math.round
-
 fun main() {
     val amount = 494876
     val minComission = 35
     val comissionRatio = 0.75
 
-    var comission = round((amount * comissionRatio) / 100)
+    var comission = (amount * comissionRatio) / 100
     if (comission < minComission) {
         comission = minComission.toDouble()
     }
-    println("Размер комиссии: $comission руб.")
+    println("Размер комиссии: ${comission.toInt()} руб.")
 }
