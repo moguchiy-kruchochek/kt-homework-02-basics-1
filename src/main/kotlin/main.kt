004eb43f26e1f78e58ded7558b55fa509c099cc1
Jaroslav Filatov <starayaglina@gmail.com>
2025-03-17 16:11:27 +0700

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+package ru.netology
+
+fun main() {
+    val amount = 494876
+    val minComission = 35
+    val comissionRatio = 0.75
+
+    var comission = (amount * comissionRatio) / 100
+    if (comission < minComission) {
+        comission = minComission.toDouble()
+    }
+    println("Размер комиссии: $comission руб.")
+}
