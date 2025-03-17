0d54b92081b6f59cb09ad7320ee7fc3a5fbb4c8e
Jaroslav Filatov <starayaglina@gmail.com>
2025-03-17 21:13:59 +0700

    round comission

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,13 @@
 package ru.netology
 
+import kotlin.math.round
+
 fun main() {
     val amount = 494876
     val minComission = 35
     val comissionRatio = 0.75
 
-    var comission = (amount * comissionRatio) / 100
+    var comission = round((amount * comissionRatio) / 100)
     if (comission < minComission) {
         comission = minComission.toDouble()
     }
